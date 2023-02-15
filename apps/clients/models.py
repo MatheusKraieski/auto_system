d460fb1faf137646c4f94802f660ed88dfd67c0a
MatheusKriaeski <gasparkraieski@gmail.com>
2023-02-15 17:40:10 -0300

    chore: create new apps

---
+++ apps/clients/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+class Client(models.Model):
+    name = models.CharField('Nome', max_length=100)
+    email = models.EmailField('E-Mail', max_length=100, unique=True)
+    cpf = models.CharField('CPF', blank=True, null=True, max_length=11, unique=True)
+    cnpj = models.CharField('CNPJ', blank=True, null=True, max_length=14, unique=True)
+    first_phone = models.CharField('Telefone', max_length=50, null=True, blank=True)
+    second_phone = models.CharField('Telefone 2', max_length=50, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
--- core/settings.py
+++ core/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'apps.cars',
+    'apps.clients',
+    'apps.services',
+
 ]
 
 MIDDLEWARE = [
