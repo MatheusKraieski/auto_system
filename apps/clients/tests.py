5b68716f2125f9a91a8ee575cf8af8f2e0b294e0
Matheus Kraieski <89086545+MatheusKraieski@users.noreply.github.com>
2023-02-22 22:32:49 -0300

    chore: test update

--- apps/clients/tests.py
+++ apps/clients/tests.py
@@ -1,4 +1,5 @@
 import pytest
+
 from django.urls import reverse
 
 
@@ -7,3 +8,18 @@ def test_client_status_code_200_ok(client):
     url = reverse('clients')
     response = client.get(url)
     assert response.status_code == 200
+
+# def test_client_db_is_correct(client):
+#     url = reverse('clients')
+#     response = client.get(url)
+#     assert response.json == [
+# 	{
+# 		"id": 1,
+# 		"name": "Matheus Kraieski",
+# 		"email": "mathkraieski@gmail.com",
+# 		"cpf": "1540620001",
+# 		"cnpj": null,
+# 		"first_phone": "51994542807",
+# 		"second_phone": null
+# 	}
+# ]
