4c318ebdc6ddb845beb75afbf0834e69b6596302
MatheusKriaeski <gasparkraieski@gmail.com>
2023-02-23 13:35:38 -0300

    chore: finishing the Client test (Amem)

--- apps/clients/tests.py
+++ apps/clients/tests.py
@@ -1,6 +1,7 @@
 import pytest
-
 from django.urls import reverse
+
+from apps.clients.models import Client
 
 
 @pytest.mark.django_db
@@ -9,17 +10,26 @@ def test_client_status_code_200_ok(client):
     response = client.get(url)
     assert response.status_code == 200
 
-# def test_client_db_is_correct(client):
-#     url = reverse('clients')
-#     response = client.get(url)
-#     assert response.json == [
-# 	{
-# 		"id": 1,
-# 		"name": "Matheus Kraieski",
-# 		"email": "mathkraieski@gmail.com",
-# 		"cpf": "1540620001",
-# 		"cnpj": null,
-# 		"first_phone": "51994542807",
-# 		"second_phone": null
-# 	}
-# ]
+
+@pytest.mark.django_db
+def test_client_db_is_correct(client):
+    Client.objects.create(
+        name="Matheus Kraieski",
+        email="mathkraieski@gmail.com",
+        cpf="1540620001",
+        cnpj="00000000000000",
+        first_phone="51994542807",
+        second_phone="51993580435"
+    )
+    response = client.get(reverse('clients'))
+    assert response.json() == [
+        {
+            "id": 1,
+            "name": "Matheus Kraieski",
+            "email": "mathkraieski@gmail.com",
+            "cpf": "1540620001",
+            "cnpj": "00000000000000",
+            "first_phone": "51994542807",
+            "second_phone": "51993580435"
+        }
+    ]
